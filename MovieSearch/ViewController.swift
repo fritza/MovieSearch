ea5607c881ccf7b8c0183f4b6ecb567e022ea7a7
Fritz Anderson <anderson.fritz@gmail.com>
2019-11-13 12:33:57 -0600

    Initial Commit

---
+++ MovieSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by Fritz Anderson on 11/13/19.
+//  Copyright © 2019 Fritz Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
