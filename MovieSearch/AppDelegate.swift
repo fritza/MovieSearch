812da474b99992a329e1a9588b5203b633fc43c3
Fritz Anderson <anderson.fritz@gmail.com>
2019-11-18 18:02:12 -0600

    Strip the Core Data experiments from the app.

--- MovieSearch/AppDelegate.swift
+++ MovieSearch/AppDelegate.swift
@@ -10,7 +10,9 @@ import UIKit
 import CoreData
 import AlamofireNetworkActivityIndicator
 
+/*
 var gManagedObjectContext: NSManagedObjectContext!
+*/
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,42 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
    // var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        gManagedObjectContext = managedObjectContext
         NetworkActivityIndicatorManager.shared.isEnabled = true
         return true
     }
-    
-    // MARK: - Core Data stack
-    
-    lazy var mangedObjectModel: NSManagedObjectModel = {
-        guard
-            let momURL = Bundle.main.url(
-                forResource: Constants.momBaseName,
-                withExtension: "mom"),
-            let retval = NSManagedObjectModel(contentsOf: momURL)
-            else { preconditionFailure() }
-        return retval
-    }()
-    
-    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        do {
-            let retval = NSPersistentStoreCoordinator(managedObjectModel: mangedObjectModel)
-            try retval.addPersistentStore(
-                ofType: NSInMemoryStoreType,
-                configurationName: nil, at: nil)
-            return retval
-        }
-        catch {
-            preconditionFailure()
-        }
-    }()
-    
-    lazy var managedObjectContext: NSManagedObjectContext = {
-        // Probably will never need background processing.
-        // The store is in-memory.
-        return NSManagedObjectContext(
-            concurrencyType: .mainQueueConcurrencyType)
-    }()
 }
 
